cad692f4259b6a40eeab43da018f8d3312e21ccd
omarAlfaSoftware.com <omar@alfa-software.com>
2025-08-28 17:14:04 +0300

    add the the test controller that recieve the productdatatype as enum and extract the data based on the type using the ProductFileService at Application layer and then call the factory to get the right productdata then generate the headers and data and send to the excel exporter to create the file at wwwroot then return the path at the response

--- src/GalaxusIntegration.Api/Controllers/FilesController.cs
+++ src/GalaxusIntegration.Api/Controllers/FilesController.cs
@@ -1,3 +1,5 @@
+using GalaxusIntegration.Application.Services;
+using GalaxusIntegration.Shared.Enum;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GalaxusIntegration.Api.Controllers
@@ -6,11 +8,31 @@ namespace GalaxusIntegration.Api.Controllers
     [Route("api/files")]
     public class FilesController : ControllerBase
     {
+        private readonly ProductFileService _productFileService;
+        public FilesController(ProductFileService productFileService)
+        {
+            _productFileService = productFileService;
+        }
         [HttpGet("processing-status")]
         public IActionResult GetProcessingStatus()
         {
-            // Return file processing status
+            
             return Ok();
+        }
+        [HttpPost("{type}")]
+        public async Task<ActionResult> UploadFile(ProductDataType type)
+        {
+            // Handle file upload
+            try
+            {
+               var result=await _productFileService.GenerateProductDataFileAsync(type);
+            return  Ok(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest(e);
+            }
         }
 
         [HttpPost("reprocess/{id}")]
--- src/GalaxusIntegration.Api/Program.cs
+++ src/GalaxusIntegration.Api/Program.cs
@@ -1,8 +1,15 @@
+using GalaxusIntegration.Application.Interfaces;
+using GalaxusIntegration.Application.Services;
+using GalaxusIntegration.Infrastructure.Excel_files;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IFileGenerationService>(provider =>
+    new ExcelExporter(Path.Combine(builder.Environment.ContentRootPath, "wwwroot")));;
+builder.Services.AddScoped(typeof(ProductFileService));
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
@@ -24,6 +31,12 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "Galaxus Integration API v1");
     });
 }
+//add wwwroot folder to serve static files
+//publish wwwroot folder with handling the path to be in the same level as the executable
+
+
+
+
 
 app.UseHttpsRedirection();
 
